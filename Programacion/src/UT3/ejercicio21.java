e7267318ab88b4373043b905385aa3bce0f25674
Ismael m <Ismael m@DESKTOP-SFIIMMD.localdomain>
2020-10-28 12:14:49 +0100

    ejercicio21

---
+++ Programacion/src/UT3/ejercicio21.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package UT3;
+import java.util.Scanner;
+/**
+ * @author Ismael m
+ *
+ */
+public class ejercicio21 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero=0;
+		int suma;
+		float contador=0;
+		
+		do {
+			System.out.println("Escribe un numero entero:");
+			Scanner scs= new Scanner(System.in);
+			suma= scs.nextInt();
+			numero= numero+suma;
+			contador=contador+1;
+		}while(suma!=0);
+		System.out.printf("la suma total es %d\n",numero);
+		System.out.printf("y la media de los numeros es %5.2f",numero/contador);
+
+	}
+
+}
