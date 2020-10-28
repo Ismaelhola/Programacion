9e49342f8645169d7c347bcf44b5eae5466e394f
Usuario <Usuario@192.168.1.138>
2020-10-28 18:54:00 +0100

    casa ejercicio10

--- Programacion/src/UT3/ejercicio10.java
+++ Programacion/src/UT3/ejercicio10.java
@@ -18,9 +18,31 @@ public class ejercicio10 {
 		float iva;
 		
 		do {
-			System.out.println("¿Que pocion quieres escojer");
-			System.out.println("1.");
-		}
+			System.out.println("¿Que opcion quieres escojer");
+			System.out.println("1. pasar de precio sin iva a con iva");
+			System.out.println("2. pasar de precio con iva a sin iva");
+			System.out.println("3. salir");
+			do {
+				System.out.println("pon la opcion que quieres escojer:");
+				Scanner sco=new Scanner(System.in);
+				opcion= sco.nextInt();
+			}while(opcion<1 && opcion>3);
+			switch(opcion) {
+				case 1:
+					System.out.println("Cual es el precio sin iva:");
+					Scanner sci1=new Scanner(System.in);
+					iva= sci1.nextFloat();
+					System.out.printf("El precio con iva es %5.2f\n",iva+(iva*0.21));
+					break;
+					
+				case 2:
+					System.out.println("Cual es el precio con iva:");
+					Scanner sci2=new Scanner(System.in);
+					iva= sci2.nextFloat();
+					System.out.printf("El precio sin iva es %5.2f\n",iva/1.21);
+					break;
+			}
+		}while(opcion!=3);
 
 	}
 
