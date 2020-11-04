8cdeeb22e63b2037bc5d4ec8d7fc0658477c535d
carlos.cg <carlos.cg@DESKTOP-3K5TKR4.localdomain>
2020-11-04 10:24:43 +0100

    ejercicio en conjunto

---
+++ Entornos/src/Ejercicios/ejercicioenconjunto.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author carlos.cg
+ *
+ */
+public class ejercicioenconjunto {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Ismael");
+	}
+
+}
