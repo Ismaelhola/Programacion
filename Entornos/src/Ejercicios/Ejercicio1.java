460c6fe901c0391a54561a37f4c200cb3cd011cc
carlos.cg <carlos.cg@DESKTOP-3K5TKR4.localdomain>
2020-11-04 09:26:57 +0100

    Ejercicio 1 de entornos

---
+++ Entornos/src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author carlos.cg
+ *
+ */
+public class Ejercicio1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.print("Hola Mundo");
+
+			System.out.print("Adios Mundo");
+	}
+
+}
