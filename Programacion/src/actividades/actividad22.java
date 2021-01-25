c0108dc10a27f78a942b6d1176e6b9c81f52a001
Ismael m <Ismael m@DESKTOP-SFIIMMD.localdomain>
2021-01-25 10:10:37 +0100

    actividad 22

--- Programacion/src/actividades/actividad22.java
+++ Programacion/src/actividades/actividad22.java
@@ -14,6 +14,11 @@ public class actividad22 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		libros libro1 = new libros("la casa de la pradera", "Chiquito de la calzada", 10, 9);
+		libros libro2 = new libros("el ultimo mohicano", "kiko rivera", 5, 4);
+		
+		System.out.println("Que operacion quieres realizar");
 
 	}
 
--- Programacion/src/actividades/libros.java
+++ Programacion/src/actividades/libros.java
@@ -19,10 +19,10 @@ public class libros {
 	}
 	
 	public libros(String t, String a, int e, int p) {
-		this.titulo=t;
-		this.autor=a;
-		this.ejemplares=e;
-		this.prestados=p;
+		settitulos(t);
+		setautor(a);
+		setejemplares(e);
+		setprestados(p);
 	}
 	
 	public String gettitulo() {
@@ -38,6 +38,44 @@ public class libros {
 		return this.prestados;
 	}
 	
+	public void settitulos(String t) {
+		this.titulo=t;
+	}
+	public void setautor(String a) {
+		this.autor=a;
+	}
+	public void setejemplares(int e) {
+		this.ejemplares=e;
+	}
+	public void setprestados(int p) {
+		this.prestados=p;
+	}
 	
-
+	public Boolean prestar() {
+		Boolean r = false;
+		if(this.prestados<this.ejemplares) {
+			r = true;
+			prestados++;
+		}else {
+			r = false;
+		}
+		return r;
+	}
+	
+	public Boolean devolver() {
+		Boolean r = false;
+		if(this.prestados>0) {
+			r = true;
+			prestados--;
+		}else {
+			r = false;
+		}
+		return r;
+	}
+	
+	@Override
+	public String toString() {
+		return "El libro"+this.titulo+" del autor "+this.autor+" tiene "+this.ejemplares+"y ahora estan prestados "+this.prestados;
+	}
+	
 }
