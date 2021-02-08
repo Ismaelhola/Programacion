8f9c84b80abb052f1a881731af6a586bd7f1b6b6
Usuario <Usuario@LAPTOP-B5DJ2DJK>
2021-02-08 17:54:14 +0100

    .

---
+++ Programacion/src/actividades/EstadisticasTest.java
@@ -0,0 +1,34 @@
+package actividades;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EstadisticasTest {
+
+	@Test
+	void testMedia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testModa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVariacionSR() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVariacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPermutaciones() {
+		fail("Not yet implemented");
+	}
+
+}
